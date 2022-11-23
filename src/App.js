6d172b94be34b32bb4deeb20773dcc14b72fb08c
ozrn <ozerenkevser92@gmail.com>
2022-11-23 22:17:49 +0300

    Create and style header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import Header from "./Header"
 
 export default function App() {
   return (
-    <h1>Building meme generator</h1>
+    <Header />
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="header">
+          
+          <img src= "/images/troll-face.png" alt = "" className="header-img"></img>
+          
+          <h2 className="header-title">Meme Generator</h2>
+        
+        </header>
+    )
+};
