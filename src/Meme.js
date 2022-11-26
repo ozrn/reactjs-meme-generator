a3163a686ccc0534327ccd5aff9a0c4f8112bbbf
ozrn <ozerenkevser92@gmail.com>
2022-11-26 13:01:09 +0300

    Update index.html, add data file for memes

--- src/Meme.js
+++ src/Meme.js
@@ -1,18 +1,18 @@
 export default function Meme() {
-    return (
-        <main>
-          
-          <form className= "form">
-            
-            <input type= "text" placeholder = "top text" className= "form-input"></input>
-            
-            <input type = "text" placeholder = "bottom text" className= "form-input"></input>
-            
-            <button className= "form-btn">Get a new meme image</button>
-          
-          </form>
+  return (
+    <main>
+      
+      <form className= "form">
         
-        </main>
+        <input type= "text" placeholder = "top text" className= "form-input"></input>
+        
+        <input type = "text" placeholder = "bottom text" className= "form-input"></input>
+        
+        <button className= "form-btn">Get a new meme image</button>
+      
+      </form>
     
-    )
-}
+    </main>
+
+)
+}   
