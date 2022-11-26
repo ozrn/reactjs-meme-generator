b2f6e449d738213441abc2d85787bd7927c5ce87
ozrn <ozerenkevser92@gmail.com>
2022-11-26 15:20:50 +0300

    Parse and stringify data to get memes array

--- src/Meme.js
+++ src/Meme.js
@@ -1,16 +1,26 @@
+import memesData from "./memesData.js"
 export default function Meme() {
+  
+  function getMemeImage() {
+    
+    const parsedData = JSON.parse(JSON.stringify(memesData)); 
+    
+    const memesArray = parsedData.data.memes
+    
+    console.log(memesArray);
+}
   return (
     <main>
       
-      <form className= "form">
+      <div className= "form">
         
         <input type= "text" placeholder = "top text" className= "form-input"></input>
         
         <input type = "text" placeholder = "bottom text" className= "form-input"></input>
         
-        <button className= "form-btn">Get a new meme image</button>
+        <button className= "form-btn" onClick = {getMemeImage} >Get a new meme image</button>
       
-      </form>
+      </div>
     
     </main>
 
